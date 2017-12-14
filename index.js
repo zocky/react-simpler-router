daefe05826bfb8a15e73546b414ec3ea5f13fcea
Zoran Obradovic <zockyo@gmail.com>
2017-12-14 22:16:37 +0000

    0.8.1 - build

--- index.js
+++
@@ -1,5 +0,0 @@
-export { Router } from './src/Router';
-export { Route, IndexRoute } from './src/Route';
-export { NavLink } from './src/NavLink';
-export { Link } from './src/Link';
-export { Redirect } from './src/Redirect';
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { Router } from './Router';
+export { Route, IndexRoute } from './Route';
+export { NavLink } from './NavLink';
+export { Link } from './Link';
+export { Redirect } from './Redirect';
